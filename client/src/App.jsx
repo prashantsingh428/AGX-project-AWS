4458ee35622bc907fe4a0479e5bd246aa9d3931a
aigrowthexa <prashant63942@gmail.com>
2026-06-09 16:00:43 +0530

    feat: remove chat widget from app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from './context/ThemeContext';
 import { NotificationProvider } from './context/NotificationContext';
 import { AuthProvider } from './context/AuthContext';
 import Notification from './components/Notification';
-import ChatWidget from './components/ChatWidget';
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
                 <AuthProvider>
                     <AppRoutes />
                     <Notification />
-                    <ChatWidget />
                     <Analytics />
                 </AuthProvider>
             </NotificationProvider>
