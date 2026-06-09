599c8e5fb119f7115c0aa254173996df6f332d77
aigrowthexa <prashant63942@gmail.com>
2026-06-09 15:49:33 +0530

    fix: reposition floating contact widget to prevent overlap with back to top button

--- client/src/components/FloatingContactWidget.jsx
+++ client/src/components/FloatingContactWidget.jsx
@@ -39,7 +39,7 @@ const FloatingContactWidget = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 font-sans">
+    <div className="fixed bottom-28 right-8 z-50 font-sans">
       <AnimatePresence>
         {isOpen && (
           <motion.div
